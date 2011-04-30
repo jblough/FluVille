0c6d9a8085e1907ed1377fb99b466f8ebf3a92f0
jblough <joe.blough@gmail.com>
2011-04-30 05:45:49 +0000

    Made some methods public/static.  Formatted the file
    git-svn-id: file:///Users/joe/test/git/flu_svndumps/FluVille/SVNROOT@16 e5a0faef-2dc9-42db-ad92-61d735235e97

--- android_apps/trunk/FluVille/src/com/josephblough/fluville/feeds/DataRetriever.java
+++ android_apps/trunk/FluVille/src/com/josephblough/fluville/feeds/DataRetriever.java
@@ -30,114 +30,114 @@ import android.util.Log;
 
 @SuppressWarnings("unchecked")
 public class DataRetriever {
-    private final static String TAG = "DataRetriever";
+	private final static String TAG = "DataRetriever";
 
-    private static final String FLU_VACCINATION_ESTIMATES_URL = "http://www.cdc.gov/flue/professionals/vaccination/reporti1011/resources/2010-11_Coverage.xls";
-    private static final String WEEKLY_FLU_ACTIVITY_REPORT_URL = "http://www.cdc.gov/flu/weekly/flureport.xml";
-    private static final String FLU_PAGES_AS_XML_URL = "http://t.cdc.gov/feed.aspx?tpc=26829&days=90";
-    private static final String FLU_PAGES_AS_JSON_URL = "http://t.cdc.gov/feed.aspx?tpc=26829&days=90&fmt=json";
-    private static final String FLU_UPDATES_URL = "http://www2c.cdc.gov/podcasts/createrss.asp?t=r&c=20";
-    private static final String FLU_PODCASTS_URL = "http://www2c.cdc.gov/podcasts/searchandcreaterss.asp?topic=flue";
-    private static final String CDC_FEATURES_PAGES_AS_XML_URL = "http://t.cdc.gov/feed.aspx?tpc=26816&fromdate=1/1/2011";
-    private static final String CDC_FEATURES_PAGES_AS_JSON_URL = "http://t.cdc.gov/feed.aspx?tpc=26816&fromdate=1/1/2011&fmt=json";
-    
-    
-    public void getFluVaccinationEstimates() {
-	// Excel spreadsheet
-	HttpClient client = new DefaultHttpClient();
-	HttpGet get = new HttpGet(FLU_VACCINATION_ESTIMATES_URL);
-	try {
-	    HttpResponse response = client.execute(get);
-	    Log.d(TAG, "Spreadsheet has " + response.getEntity().getContentLength() + " bytes");
-	} catch (IOException e) {
-	    Log.e(TAG, e.getMessage(), e);
-	}
-    }
-    
-    public FluReport getFluActivityReport() {
-	Log.d(TAG, "Flu activity report:");
-	try {
-	    HttpClient client = new DefaultHttpClient();
-	    HttpGet httpMethod = new HttpGet(WEEKLY_FLU_ACTIVITY_REPORT_URL);
-	    HttpResponse response = client.execute(httpMethod);
-	    HttpEntity entity = response.getEntity();
+	private static final String FLU_VACCINATION_ESTIMATES_URL = "http://www.cdc.gov/flue/professionals/vaccination/reporti1011/resources/2010-11_Coverage.xls";
+	private static final String WEEKLY_FLU_ACTIVITY_REPORT_URL = "http://www.cdc.gov/flu/weekly/flureport.xml";
+	private static final String FLU_PAGES_AS_XML_URL = "http://t.cdc.gov/feed.aspx?tpc=26829&days=90";
+	private static final String FLU_PAGES_AS_JSON_URL = "http://t.cdc.gov/feed.aspx?tpc=26829&days=90&fmt=json";
+	private static final String FLU_UPDATES_URL = "http://www2c.cdc.gov/podcasts/createrss.asp?t=r&c=20";
+	private static final String FLU_PODCASTS_URL = "http://www2c.cdc.gov/podcasts/searchandcreaterss.asp?topic=flue";
+	private static final String CDC_FEATURES_PAGES_AS_XML_URL = "http://t.cdc.gov/feed.aspx?tpc=26816&fromdate=1/1/2011";
+	private static final String CDC_FEATURES_PAGES_AS_JSON_URL = "http://t.cdc.gov/feed.aspx?tpc=26816&fromdate=1/1/2011&fmt=json";
 
-	    SAXParserFactory parserFactory = SAXParserFactory.newInstance();
-	    SAXParser parser = parserFactory.newSAXParser();
-	    XMLReader reader = parser.getXMLReader();
-	    WeeklyFluActivityXmlHandler handler = new WeeklyFluActivityXmlHandler();
-	    reader.setContentHandler(handler);
-	    reader.parse(new InputSource(entity.getContent()));
-	    Log.d(TAG, "Report: " + handler.report.subtitle);
-	    Log.d(TAG, "Report has " + handler.report.periods.size() + " time periods");
-	    return handler.report;
+
+	public void getFluVaccinationEstimates() {
+		// Excel spreadsheet
+		HttpClient client = new DefaultHttpClient();
+		HttpGet get = new HttpGet(FLU_VACCINATION_ESTIMATES_URL);
+		try {
+			HttpResponse response = client.execute(get);
+			Log.d(TAG, "Spreadsheet has " + response.getEntity().getContentLength() + " bytes");
+		} catch (IOException e) {
+			Log.e(TAG, e.getMessage(), e);
+		}
 	}
-	catch (Exception e) {
-	    Log.e(TAG, e.getMessage(), e);
+
+	public static FluReport getFluActivityReport() {
+		Log.d(TAG, "Flu activity report:");
+		try {
+			HttpClient client = new DefaultHttpClient();
+			HttpGet httpMethod = new HttpGet(WEEKLY_FLU_ACTIVITY_REPORT_URL);
+			HttpResponse response = client.execute(httpMethod);
+			HttpEntity entity = response.getEntity();
+
+			SAXParserFactory parserFactory = SAXParserFactory.newInstance();
+			SAXParser parser = parserFactory.newSAXParser();
+			XMLReader reader = parser.getXMLReader();
+			WeeklyFluActivityXmlHandler handler = new WeeklyFluActivityXmlHandler();
+			reader.setContentHandler(handler);
+			reader.parse(new InputSource(entity.getContent()));
+			Log.d(TAG, "Report: " + handler.report.subtitle);
+			Log.d(TAG, "Report has " + handler.report.periods.size() + " time periods");
+			return handler.report;
+		}
+		catch (Exception e) {
+			Log.e(TAG, e.getMessage(), e);
+		}
+		return new FluReport();
 	}
-	return new FluReport();
-    }
-    
-    public List<SyndEntry> getFluPagesAsXml() {
-	Log.d(TAG, "Flu pages:");
-	return retrieveXmlRssFeed(FLU_PAGES_AS_XML_URL);
-    }
-    
-    public JSONArray getFluPagesAsJson() {
-	Log.d(TAG, "Flu pages:");
-	return retrieveJsonRssFeed(FLU_PAGES_AS_JSON_URL);
-    }
-    
-    public List<SyndEntry> getFluUpdates() {
-	Log.d(TAG, "Flu updates:");
-	return retrieveXmlRssFeed(FLU_UPDATES_URL);
-    }
-    
-    public List<SyndEntry> getFluPodcasts() {
-	Log.d(TAG, "Flu podcasts:");
-	return retrieveXmlRssFeed(FLU_PODCASTS_URL);
-    }
-    
-    public List<SyndEntry> getCdcFeaturesPagesAsXml() {
-	Log.d(TAG, "CDC Features pages:");
-	return retrieveXmlRssFeed(CDC_FEATURES_PAGES_AS_XML_URL);
-    }
-    
-    public JSONArray getCdcFeaturesPagesAsJson() {
-	Log.d(TAG, "CDC Features pages:");
-	return retrieveJsonRssFeed(CDC_FEATURES_PAGES_AS_JSON_URL);
-    }
-    
-    private List<SyndEntry> retrieveXmlRssFeed(final String url) {
-	try {
-	    URL feedUrl = new URL(url);
-	    SyndFeedInput input = new SyndFeedInput();
-	    SyndFeed feed = input.build(new XmlReader(feedUrl));
-	    /*Log.d(TAG, "SyndFeed Array has " + feed.getEntries().size() + " elements");
+
+	public List<SyndEntry> getFluPagesAsXml() {
+		Log.d(TAG, "Flu pages:");
+		return retrieveXmlRssFeed(FLU_PAGES_AS_XML_URL);
+	}
+
+	public JSONArray getFluPagesAsJson() {
+		Log.d(TAG, "Flu pages:");
+		return retrieveJsonRssFeed(FLU_PAGES_AS_JSON_URL);
+	}
+
+	public List<SyndEntry> getFluUpdates() {
+		Log.d(TAG, "Flu updates:");
+		return retrieveXmlRssFeed(FLU_UPDATES_URL);
+	}
+
+	public List<SyndEntry> getFluPodcasts() {
+		Log.d(TAG, "Flu podcasts:");
+		return retrieveXmlRssFeed(FLU_PODCASTS_URL);
+	}
+
+	public List<SyndEntry> getCdcFeaturesPagesAsXml() {
+		Log.d(TAG, "CDC Features pages:");
+		return retrieveXmlRssFeed(CDC_FEATURES_PAGES_AS_XML_URL);
+	}
+
+	public JSONArray getCdcFeaturesPagesAsJson() {
+		Log.d(TAG, "CDC Features pages:");
+		return retrieveJsonRssFeed(CDC_FEATURES_PAGES_AS_JSON_URL);
+	}
+
+	public static List<SyndEntry> retrieveXmlRssFeed(final String url) {
+		try {
+			URL feedUrl = new URL(url);
+			SyndFeedInput input = new SyndFeedInput();
+			SyndFeed feed = input.build(new XmlReader(feedUrl));
+			/*Log.d(TAG, "SyndFeed Array has " + feed.getEntries().size() + " elements");
 	    List<SyndEntry> entries = (List<SyndEntry>)feed.getEntries();
 	    for (SyndEntry entry : entries) {
 		Log.d(TAG, entry.getTitle());
 	    }*/
-	    return feed.getEntries();
+			return feed.getEntries();
+		}
+		catch (Exception e) {
+			Log.e(TAG, e.getMessage(), e);
+		}
+		return new ArrayList<SyndEntry>();
 	}
-	catch (Exception e) {
-	    Log.e(TAG, e.getMessage(), e);
+
+	public static JSONArray retrieveJsonRssFeed(final String url) {
+		HttpClient client = new DefaultHttpClient();
+		HttpGet get = new HttpGet(url);
+		try {
+			ResponseHandler<String> handler = new BasicResponseHandler();
+			String response = client.execute(get, handler);
+			return new JSONArray(response);
+		} catch (JSONException e) {
+			Log.e(TAG, e.getMessage(), e);
+		} catch (IOException e) {
+			Log.e(TAG, e.getMessage(), e);
+		}
+		return new JSONArray();
 	}
-	return new ArrayList<SyndEntry>();
-    }
-    
-    private JSONArray retrieveJsonRssFeed(final String url) {
-	HttpClient client = new DefaultHttpClient();
-	HttpGet get = new HttpGet(url);
-	try {
-	    ResponseHandler<String> handler = new BasicResponseHandler();
-	    String response = client.execute(get, handler);
-	    return new JSONArray(response);
-	} catch (JSONException e) {
-	    Log.e(TAG, e.getMessage(), e);
-	} catch (IOException e) {
-	    Log.e(TAG, e.getMessage(), e);
-	}
-	return new JSONArray();
-    }
 }
