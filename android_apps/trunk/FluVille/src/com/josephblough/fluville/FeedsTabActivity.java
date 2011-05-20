e0e2794a27ffcf4998a88de02c0df55cd44e76a5
jblough <joe.blough@gmail.com>
2011-05-20 16:23:18 +0000

    Updated the icon to have a larger CDC logo.  Added icons to the CDC information feeds tabs.
    git-svn-id: file:///Users/joe/test/git/flu_svndumps/FluVille/SVNROOT@45 e5a0faef-2dc9-42db-ad92-61d735235e97

--- android_apps/trunk/FluVille/src/com/josephblough/fluville/FeedsTabActivity.java
+++ android_apps/trunk/FluVille/src/com/josephblough/fluville/FeedsTabActivity.java
@@ -23,27 +23,21 @@ public class FeedsTabActivity extends TabActivity {
 
 		intent = new Intent().setClass(this, FeedActivity.class);
 		intent.putExtra(FeedActivity.FEED_EXTRA, FeedActivity.FLU_PAGES);
-		//spec = tabHost.newTabSpec("words").setIndicator("Words", res.getDrawable(R.drawable.index_cards)).setContent(intent);
-		spec = tabHost.newTabSpec("pages").setIndicator("Pages").setContent(intent);
+		spec = tabHost.newTabSpec("pages").setIndicator("Flu Pages", res.getDrawable(R.drawable.flu_pages_tab)).setContent(intent);
 		tabHost.addTab(spec);
 
 		intent = new Intent().setClass(this, FeedActivity.class);
 		intent.putExtra(FeedActivity.FEED_EXTRA, FeedActivity.FLU_UPDATES);
-		//spec = tabHost.newTabSpec("wordlists").setIndicator("Word Lists", res.getDrawable(R.drawable.shoebox)).setContent(intent);
-		spec = tabHost.newTabSpec("updates").setIndicator("Updates").setContent(intent);
+		spec = tabHost.newTabSpec("updates").setIndicator("Flu Updates", res.getDrawable(R.drawable.flu_updates_tab)).setContent(intent);
 		tabHost.addTab(spec);
 
-		//intent = new Intent().setClass(this, FeedActivity.class);
-		//intent.putExtra(FeedActivity.FEED_EXTRA, FeedActivity.FLU_PODCASTS);
 		intent = new Intent().setClass(this, FluPodcasts.class);
-		//spec = tabHost.newTabSpec("download").setIndicator("Download", res.getDrawable(R.drawable.inbox)).setContent(intent);
-		spec = tabHost.newTabSpec("podcasts").setIndicator("Podcasts").setContent(intent);
+		spec = tabHost.newTabSpec("podcasts").setIndicator("Flu Podcasts", res.getDrawable(R.drawable.flu_podcasts_tab)).setContent(intent);
 		tabHost.addTab(spec);
 
 		intent = new Intent().setClass(this, FeedActivity.class);
 		intent.putExtra(FeedActivity.FEED_EXTRA, FeedActivity.CDC_FEATURE_PAGES);
-		//spec = tabHost.newTabSpec("download").setIndicator("Download", res.getDrawable(R.drawable.inbox)).setContent(intent);
-		spec = tabHost.newTabSpec("features").setIndicator("CDC\nFeature Pages").setContent(intent);
+		spec = tabHost.newTabSpec("features").setIndicator("CDC Feature Pages", res.getDrawable(R.drawable.cdc_feature_pages_tab)).setContent(intent);
 		tabHost.addTab(spec);
 
 		tabHost.setCurrentTab(0);
